156c7f9cc482e375f743b59c994de3115321f450
nataliabieniek <natalia.bieniek@student.pk.edu.pl>
2020-12-10 12:22:55 +0100

    budowanie w cmake

---
+++ build/CMakeFiles/test.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Natalia Bieniek/Desktop/studia/3 sem/jipp_2/macierz/src/main.cpp" "C:/Users/Natalia Bieniek/Desktop/studia/3 sem/jipp_2/macierz/build/CMakeFiles/test.dir/src/main.cpp.obj"
+  "C:/Users/Natalia Bieniek/Desktop/studia/3 sem/jipp_2/macierz/src/matrix.cpp" "C:/Users/Natalia Bieniek/Desktop/studia/3 sem/jipp_2/macierz/build/CMakeFiles/test.dir/src/matrix.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././src"
+  ".././include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
